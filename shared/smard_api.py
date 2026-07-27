ea084efd61ba56dbc7347cf436723374788d61ab
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-27 10:34:57 +0200

    [ADD] classes for typing in smard_api.py.

--- shared/smard_api.py
+++ shared/smard_api.py
@@ -4,3 +4,12 @@ import requests
 from typing import TypedDict
 
 from config import BASE_URL
+
+# Define classes
+class MetaData(TypedDict):
+    version: int
+    created: int
+
+class SmardBlock(TypedDict):
+    meta_data: MetaData
+    series: list[list[int | float | None]]
