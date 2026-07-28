1383b7f4c23b7dc99f574571ba067bfa8aa4a95b
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-28 12:09:07 +0200

    [REFACTOR] FILTERS to CATEGORIES.

--- smard_pipeline/config.py
+++ smard_pipeline/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import TypedDict
 
 ROOT_DIR = Path(__file__).parent.parent
 DATA_DIR = ROOT_DIR / "data"
@@ -6,54 +7,198 @@ RAW_DATA_DIR = DATA_DIR / "raw_data"
 PREPROCESSED_DATA_DIR = DATA_DIR / "preprocessed_data"
 LOGS_DIR = DATA_DIR / "logs"
 
+# Define classes
+class TimeSeriesConfig(TypedDict):
+    id: int
+    convert_timestamps: bool
+
+class EnergyTimeSeriesConfig(TimeSeriesConfig):
+    active: bool
+    deprecated_since: str | None
+
 # base url of the SMARD API.
 BASE_URL: str = "https://www.smard.de/app"
 
-FILTERS: dict[str, dict[str, int]] = {
+CATEGORIES: dict[str, dict[str, TimeSeriesConfig | EnergyTimeSeriesConfig]] = {
     'Stromerzeugung': {
-        'Braunkohle': 1223,
-        'Kernenergie': 1224,
-        'Wind Offshore': 1225,
-        'Wasserkraft': 1226,
-        'Sonstige Konventionelle': 1227,
-        'Sonstige Erneuerbare': 1228,
-        'Biomasse': 4066,
-        'Wind Onshore': 4067,
-        'Photovoltaik': 4068,
-        'Steinkohle': 4069,
-        'Pumpspeicher': 4070,
-        'Erdgas': 4071,
+        'Braunkohle': EnergyTimeSeriesConfig(
+            id=1223,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Kernenergie': EnergyTimeSeriesConfig(
+            id=1224,
+            convert_timestamps=True,
+            active=False,
+            deprecated_since="2023-04-16 0:00"
+        ),
+        'Wind Offshore': EnergyTimeSeriesConfig(
+            id=1225,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Wasserkraft': EnergyTimeSeriesConfig(
+            id=1226,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Sonstige Konventionelle': EnergyTimeSeriesConfig(
+            id=1227,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Sonstige Erneuerbare': EnergyTimeSeriesConfig(
+            id=1228,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Biomasse': EnergyTimeSeriesConfig(
+            id=4066,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Wind Onshore': EnergyTimeSeriesConfig(
+            id=4067,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Photovoltaik': EnergyTimeSeriesConfig(
+            id=4068,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Steinkohle': EnergyTimeSeriesConfig(
+            id=4069,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Pumpspeicher': EnergyTimeSeriesConfig(
+            id=4070,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
+        'Erdgas': EnergyTimeSeriesConfig(
+            id=4071,
+            convert_timestamps=False,
+            active=True,
+            deprecated_since=None
+        ),
     },
     'Stromverbrauch': {
-        'Gesamt (Netzlast)': 410,
-        'Residuallast': 4359,
-        'Pumpspeicher': 4387
+        'Gesamt (Netzlast)': TimeSeriesConfig(
+            id=410,
+            convert_timestamps=False
+        ),
+        'Residuallast': TimeSeriesConfig(
+            id=4359,
+            convert_timestamps=False
+        ),
+        'Pumpspeicher': TimeSeriesConfig(
+            id=4387,
+            convert_timestamps=False
+        )
     },
     'Marktpreis': {
-        'Deutschland-Luxemburg': 4169,
-        'Anrainer DE-LU': 5078,
-        'Belgien': 4996,
-        'Norwegen 2': 4997,
-        'Österreich': 4170,
-        'Dänemark 1': 252,
-        'Dänemark 2': 253,
-        'Frankreich': 254,
-        'Italien (Nord)': 255,
-        'Niederlande': 256,
-        'Polen 1': 257,
-        'Polen 2': 258,
-        'Schweiz': 259,
-        'Slowenien': 260,
-        'Tschechien': 261,
-        'Ungarn': 262
+        'Deutschland-Luxemburg': TimeSeriesConfig(
+            id=4169,
+            convert_timestamps=True
+        ),
+        'Anrainer DE-LU': TimeSeriesConfig(
+            id=5078,
+            convert_timestamps=True
+        ),
+        'Belgien': TimeSeriesConfig(
+            id=4996,
+            convert_timestamps=True
+        ),
+        'Norwegen 2': TimeSeriesConfig(
+            id=4997,
+            convert_timestamps=True
+        ),
+        'Österreich': TimeSeriesConfig(
+            id=4170,
+            convert_timestamps=True
+        ),
+        'Dänemark 1': TimeSeriesConfig(
+            id=252,
+            convert_timestamps=True
+        ),
+        'Dänemark 2': TimeSeriesConfig(
+            id=253,
+            convert_timestamps=True
+        ),
+        'Frankreich': TimeSeriesConfig(
+            id=254,
+            convert_timestamps=True
+        ),
+        'Italien (Nord)': TimeSeriesConfig(
+            id=255,
+            convert_timestamps=True
+        ),
+        'Niederlande': TimeSeriesConfig(
+            id=256,
+            convert_timestamps=True
+        ),
+        'Polen 1': TimeSeriesConfig(
+            id=257,
+            convert_timestamps=True
+        ),
+        'Polen 2': TimeSeriesConfig(
+            id=258,
+            convert_timestamps=True
+        ),
+        'Schweiz': TimeSeriesConfig(
+            id=259,
+            convert_timestamps=True
+        ),
+        'Slowenien': TimeSeriesConfig(
+            id=260,
+            convert_timestamps=True
+        ),
+        'Tschechien': TimeSeriesConfig(
+            id=261,
+            convert_timestamps=True
+        ),
+        'Ungarn': TimeSeriesConfig(
+            id=262,
+            convert_timestamps=True
+        )
     },
     'Prognostizierte Erzeugung': {
-        'Offshore': 3791,
-        'Onshore': 123,
-        'Photovoltaik': 125,
-        'Sonstige': 715,
-        'Wind und Photovoltaik': 5097,
-        'Gesamt': 122
+        'Offshore': TimeSeriesConfig(
+            id=3791,
+            convert_timestamps=False
+        ),
+        'Onshore': TimeSeriesConfig(
+            id=123,
+            convert_timestamps=False
+        ),
+        'Photovoltaik': TimeSeriesConfig(
+            id=125,
+            convert_timestamps=False
+        ),
+        'Sonstige': TimeSeriesConfig(
+            id=715,
+            convert_timestamps=False
+        ),
+        'Wind und Photovoltaik': TimeSeriesConfig(
+            id=5097,
+            convert_timestamps=False
+        ),
+        'Gesamt': TimeSeriesConfig(
+            id=122,
+            convert_timestamps=False
+        )
     }
 }
 
@@ -85,95 +230,4 @@ PATH_DICT: dict[str, str] = {
     'Stromverbrauch': 'power_consumption',
     'Marktpreis': 'market_price',
     'Prognostizierte Erzeugung': 'forecasted_generation'
-}
-
-ENERGY_CATEGORIES: dict[str, dict] = {
-    # Active energies
-    'Braunkohle': {
-        'filter_id': 1223,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Wind Offshore': {
-        'filter_id': 1225,
-        'active': True, 
-        'deprecated_since': None
-    },
-    'Wasserkraft': {
-        'filter_id': 1226,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Sonstige Konventionelle': {
-        'filter_id': 1227,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Sonstige Erneuerbare': {
-        'filter_id': 1228,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Biomasse': {
-        'filter_id': 4066,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Wind Onshore': {
-        'filter_id': 4067,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Photovoltaik': {
-        'filter_id': 4068,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Steinkohle': {
-        'filter_id': 4069,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Pumpspeicher': {
-        'filter_id': 4070,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Erdgas': {
-        'filter_id': 4071,
-        'active': True,
-        'deprecated_since': None
-    },
-    # Inactive energies
-    'Kernenergie': {
-        'filter_id': 1224,
-        'active': False,
-        'deprecated_since': "2023-04-16 0:00"
-    },
-}
-
-CONSUMPTION_CATEGORIES: dict[str, dict] = {
-    'Gesamt (Netzlast)': {
-        'filter_id': 410,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Residuallast': {
-        'filter_id': 4359,
-        'active': True,
-        'deprecated_since': None
-    },
-    'Pumpspeicher': {
-        'filter_id': 4387,
-        'active': False,
-        'deprecated_since': None
-    }
-}
-
-PRICE_CATEGORIES: dict[str, dict] = {
-    'Deutschland-Luxemburg': {
-        'filter_id': 4169,
-        'active': True,
-        'deprecated_since': None
-    }
 }
