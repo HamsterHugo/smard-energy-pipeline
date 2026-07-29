2afe16596768edd11d063ceba376b209c8e849be
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-29 21:59:34 +0200

    [ADD] account id for s3 bucket name.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
   name_prefix = "smard-energy-pipeline"
+  account_id = data.aws_caller_identity.current.account_id
 
   common_tags = {
     Owner = var.owner
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
+data "aws_caller_identity" "current" {}
+
 module "data_bucket" {
   source = "./modules/s3"
 
-  bucket_name = "${local.name_prefix}_data_bucket"
+  bucket_name = "${local.name_prefix}_data_bucket_${local.account_id}"
   tags        = local.common_tags
 }
