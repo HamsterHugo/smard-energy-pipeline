918e57970d061887f94cf8f247a31ee94414f708
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-31 15:57:42 +0200

    [FIX] add some spaces in the output of examples for better readability.

--- local/01_download_historical.py
+++ local/01_download_historical.py
@@ -6,10 +6,10 @@ from downloader import update_raw_data
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print("Usage: python 01_download_historical.py <category> <subcategory>")
-        print("Example: python 01_download_historical.py Stromerzeugung Erdgas")
-        print("Example: python 01_download_historical.py Stromverbrauch Residuallast")
+        print("    Example: python 01_download_historical.py Stromerzeugung Erdgas")
+        print("    Example: python 01_download_historical.py Stromverbrauch Residuallast")
         print('For looping through all subcategories use:')
-        print('python 01_download_historical.py <category> all')
+        print('    python 01_download_historical.py <category> all')
         sys.exit(1)
 
     category = sys.argv[1]
