0bce1c5632745ca6c124be37ade1d2ae1421ff9d
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-02 19:39:07 +0200

    [REFACTOR] combined all arguments into the file main.py.

---
+++ local/main.py
@@ -0,0 +1,48 @@
+import sys
+
+from downloader import update_raw_data, download_current_week
+from smard_pipeline.config import CATEGORIES
+from smard_pipeline.transformations import merge_raw_data, merge_all_categories
+
+usage: str = """
+USAGE:
+    python main.py historical <category> <subcategory>
+    python main.py historical <category> all
+    python main.py historical combine
+    pyhton main.py merge <category> <subcategory>
+    python main.py merge <category> all
+    python main.py current <category> <subcategory>
+    python main.py current <category> all
+    python main.py current combine
+"""
+
+functions = {
+    'historical': update_raw_data,
+    'merge': merge_raw_data,
+    'current': download_current_week
+}
+
+if __name__ == '__main__':
+    if (len(sys.argv) not in [3,4]
+        or sys.argv[1] not in functions
+        or (sys.argv[2] == 'combine'
+            and sys.argv[1] not in ['historical', 'current'])):
+        print(usage)
+        sys.exit(1)
+
+    if len(sys.argv) == 4:
+        category: str = sys.argv[2]
+        subcategory: str = sys.argv[3]
+        f: function = functions[sys.argv[1]]
+
+        if subcategory == 'all':
+            for current_subcategory in CATEGORIES[category]:
+                f(category, current_subcategory)
+        else:
+            f(category, subcategory)
+    else:
+        if sys.argv[1] == 'historical':
+            merge_all_categories()
+        elif sys.argv[1] == 'current':
+            print('TODO: Implement function')
+
