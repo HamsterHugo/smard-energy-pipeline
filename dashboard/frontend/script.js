a6ce66f375af1ede2a309cbac965463990c79282
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-03 11:41:30 +0200

    [ADD] start date and nuclear end date in script.js.

--- dashboard/frontend/script.js
+++ dashboard/frontend/script.js
@@ -4,3 +4,6 @@ const isLocal = window.location.hostname === 'localhost' ||
 const API_URL = isLocal
     ? 'http://localhost:5000'
     : 'https://PLACEHOLDER.execute-api.us-east-1.amazonaws.com';
+
+const DATA_START_DATE = '2015-01-01';
+const NUCLEAR_END_DATE = '2023-04-16';
