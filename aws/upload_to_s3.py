a97bcaeb9037c17472eee6267bb050ff059174e9
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-05 16:59:54 +0200

    [ADD] content type to upload_to_s3.py for the frontend files.

--- aws/upload_to_s3.py
+++ aws/upload_to_s3.py
@@ -11,21 +11,31 @@ ACCOUNT_ID: str = boto3.client('sts').get_caller_identity()['Account']
 BUCKET_NAME: str = f'smard-energy-pipeline-data-bucket-{ACCOUNT_ID}'
 FRONTEND_DIR: Path = Path(__file__).parent.parent / 'dashboard' / 'frontend'
 
-def upload_file(local_path: Path, s3_key: str) -> bool:
+def upload_file(local_path: Path, s3_key: str, ConentType: str = None) -> bool:
     """Uploads a single file to S3.
 
     Args:
         local_path (Path): Local path to the file.
         s3_key (str): Target key in S3.
+        ConentType (str, optional): The ContentType needed for the browser.
+            Defaults to None.
 
     Returns:
-        bool: True if upload was successful, False otherwise.
+        bool: _description_
     """
     if not local_path.exists():
         logger.warning(f"File not found: {local_path.name}. Skipping...")
         return False
     try:
-        s3.upload_file(str(local_path), BUCKET_NAME, s3_key)
+        if ConentType is not None:
+            s3.upload_file(
+                str(local_path),
+                BUCKET_NAME,
+                s3_key,
+                ExtraArgs={'ContentType': ConentType}
+            )
+        else:
+            s3.upload_file(str(local_path), BUCKET_NAME, s3_key)
         logger.info(
             f'{local_path.name} -> s3://{BUCKET_NAME}/{s3_key}',
             extra={"status": "success"}
@@ -50,10 +60,15 @@ def upload_historical_data() -> None:
 
 def upload_frontend() -> None:
     logger.info(f"Uploading frontend files...")
-    frontend_files = ["index.html", "style.css", "script.js", "favicon.ico"]
+    frontend_files = [
+        ("index.html", 'text/html'),
+        ("style.css", 'text/css'),
+        ("script.js", 'application/javascript'),
+        ("favicon.ico", 'image/x-icon')
+    ]
     counter: int = 0
-    for file in frontend_files:
-        success = upload_file(FRONTEND_DIR / file, file)
+    for file, ContentType in frontend_files:
+        success = upload_file(FRONTEND_DIR / file, file, ConentType=ContentType)
         if success: counter += 1
 
     if counter < 4:
@@ -63,7 +78,7 @@ def upload_frontend() -> None:
 
 if __name__ == '__main__':
     setup_logging(level=logging.INFO)
-    logger: logging.Logger = logging.getLogger(__name__)
+    logger: logging.Logger = logging.getLogger('upload_to_s3')
     s3 = boto3.client('s3')
 
     upload_historical_data()
