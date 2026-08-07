5319e73077c42497724d7d34020a2cf4e2fab159
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-07 10:34:31 +0200

    [ADD] new resources in main.tf in terraform root directory.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,33 @@ module "data_bucket" {
   tags        = local.common_tags
   public      = true
 }
+
+module "lambda_ingest" {
+  source        = "./modules/lambda"
+  function_name = "${local.name_prefix}-ingest"
+  source_dir    = "${path.root}/../aws/lambda/ingest"
+  tags          = local.common_tags
+}
+
+module "lambda_query" {
+  source        = "./modules/lambda"
+  function_name = "${local.name_prefix}-query"
+  source_dir    = "${path.root}/../aws/lambda/query"
+  tags          = local.common_tags
+}
+
+module "api_gateway" {
+  source               = "./modules/api_gateway"
+  api_name             = "${local.name_prefix}-api"
+  lambda_invoke_arn    = module.lambda_query.invoke_arn
+  lambda_function_name = module.lambda_query.function_name
+  tags                 = local.common_tags
+}
+
+module "eventbridge" {
+  source               = "./modules/eventbridge"
+  rule_name            = "${local.name_prefix}-daily-ingest"
+  lambda_arn           = module.lambda_ingest.function_arn
+  lambda_function_name = module.lambda_ingest.function_name
+  tags                 = local.common_tags
+}
