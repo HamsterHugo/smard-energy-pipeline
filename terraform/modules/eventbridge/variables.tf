a4e2e6f765fa9008432f86523b605df0474d70b9
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-07 10:24:57 +0200

    [ADD variables.tf to terraform module eventbridge.

---
+++ terraform/modules/eventbridge/variables.tf
@@ -0,0 +1,26 @@
+variable "rule_name" {
+  description = "Name of the EventBridge rule."
+  type = string
+}
+
+variable "schedule_expression" {
+  description = "Schedule expression for the EventBridge rule."
+  type = string
+  default = "cron(0 2 * * ? *)"
+}
+
+variable "lambda_arn" {
+  description = "ARN of the Lambda function to trigger."
+  type = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function."
+  type = string
+}
+
+variable "tags" {
+  description = "Tags to aplpy to the EventBridge rule."
+  type = map(string)
+  default = {}
+}
