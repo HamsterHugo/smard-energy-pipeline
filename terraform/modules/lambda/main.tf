09e1ddaca06a74482a0f677b800185c01108eacb
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-07 09:47:54 +0200

    [ADD] data and resources to main.tf in terraform module lambda.

---
+++ terraform/modules/lambda/main.tf
@@ -0,0 +1,32 @@
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = var.source_dir
+  output_path = "${path.module}/tmp/${var.function_name}.zip"
+}
+
+resource "aws_lambda_function" "function" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = var.function_name
+  role             = data.aws_iam_role.lab_role.arn
+  handler          = var.handler
+  runtime          = var.runtime
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = var.environment_variables
+  }
+
+  tags = var.tags
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 7
+  tags              = var.tags
+}
