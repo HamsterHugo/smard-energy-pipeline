b7ba3147c518e90926cd8074388e1437d999917e
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-07 10:45:20 +0200

    [UPDATE] formate all terraform files.

--- terraform/modules/eventbridge/variables.tf
+++ terraform/modules/eventbridge/variables.tf
@@ -1,26 +1,26 @@
 variable "rule_name" {
   description = "Name of the EventBridge rule."
-  type = string
+  type        = string
 }
 
 variable "schedule_expression" {
   description = "Schedule expression for the EventBridge rule."
-  type = string
-  default = "cron(0 2 * * ? *)"
+  type        = string
+  default     = "cron(0 2 * * ? *)"
 }
 
 variable "lambda_arn" {
   description = "ARN of the Lambda function to trigger."
-  type = string
+  type        = string
 }
 
 variable "lambda_function_name" {
   description = "Name of the Lambda function."
-  type = string
+  type        = string
 }
 
 variable "tags" {
   description = "Tags to aplpy to the EventBridge rule."
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
