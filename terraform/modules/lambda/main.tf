19f5c5635f1fac2e96267c9150c37c4f2a3c4ceb
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-12 15:32:08 +0200

    [ADD] terraform module for lambda function.

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,7 +1,3 @@
-data "aws_iam_role" "lambda_role" {
-  name = "smard-lambda-role"
-}
-
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = var.source_dir
@@ -11,12 +7,13 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "function" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = var.function_name
-  role             = data.aws_iam_role.lambda_role.arn
+  role             = var.role_arn
   handler          = var.handler
   runtime          = var.runtime
   timeout          = var.timeout
   memory_size      = var.memory_size
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  layers           = [var.layer_arn]
 
   environment {
     variables = var.environment_variables
@@ -30,3 +27,56 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
   retention_in_days = 7
   tags              = var.tags
 }
+
+#data "aws_iam_role" "lambda_role" {
+#  name = var.lambda_role_name
+#}
+
+# ZIP file for the lambda layer
+#data "archive_file" "layer_zip" {
+#  type        = "zip"
+#  source_file = "${path.root}/../layer.zip"
+#  output_path = "${path.module}/tmp/layer.zip"
+#}
+
+# Load ZIP file into the S3 bucket.
+#resource "aws_s3_object" "layer_zip" {
+#  bucket = var.bucket_name
+#  key    = "layer.zip"
+#  source = data.archive_file.layer_zip.output_path
+#  etag   = filemd5(data.archive_file.layer_zip.output_path)
+#}
+
+# Create Lambda-Layer from the S3 bucket.
+#resource "aws_lambda_layer_version" "smard_layer" {
+#  s3_bucket           = aws_s3_object.layer_zip.bucket
+#  s3_key              = aws_s3_object.layer_zip.key
+#  layer_name          = "smard-pipeline-layer"
+#  compatible_runtimes = ["python3.12"]
+#}
+
+# Lambda function
+#resource "aws_lambda_function" "function" {
+#  filename         = data.archive_file.lambda_zip.output_path
+#  function_name    = var.function_name
+#  role             = module.iam.lambda_role_arn
+#  handler          = var.handler
+#  runtime          = var.runtime
+#  timeout          = var.timeout
+#  memory_size      = var.memory_size
+#  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#  layers           = [aws_lambda_layer_version.smard_layer.arn]
+#
+#  environment {
+#    variables = var.environment_variables
+#  }
+#
+#  tags = var.tags
+#}
+
+# CloudWatch Log group for the lambda function.
+#resource "aws_cloudwatch_log_group" "lambda_logs" {
+#  name              = "/aws/lambda/${var.function_name}"
+#  retention_in_days = 7
+#  tags              = var.tags
+#}
--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,10 +1,10 @@
 variable "function_name" {
-  description = "Nsme of the Lambda function."
+  description = "Name of the Lambda function."
   type        = string
 }
 
 variable "handler" {
-  description = "Handler for the Lambda function."
+  description = "Handler for the Lambda fucntion."
   type        = string
   default     = "handler.lambda_handler"
 }
@@ -16,35 +16,96 @@ variable "runtime" {
 }
 
 variable "timeout" {
-  description = "Timeout for the Lambda function in seconds."
+  description = "Timeout in seconds."
   type        = number
   default     = 300
 }
 
 variable "memory_size" {
-  description = "Memory size for the Lambda function in MB."
+  description = "Memory size in MB."
   type        = number
   default     = 256
 }
 
 variable "source_dir" {
-  description = "Path to the Lmabda function source directory."
+  description = "Path to the Lambda function source directory."
+  type        = string
+}
+
+variable "role_arn" {
+  description = "ARN of the IAM role."
+  type        = string
+}
+
+variable "layer_arn" {
+  description = "ARN of the Lambda Layer."
   type        = string
 }
 
 variable "environment_variables" {
-  description = "Environmen variables for the Lambda function."
+  description = "Environment variables for the Lambda function."
   type        = map(string)
   default     = {}
 }
 
 variable "tags" {
-  description = "Tags to apply to the Lambda function."
+  description = "Tags to apply to all resources."
   type        = map(string)
   default     = {}
 }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket the Lambda function needs access to."
-  type        = string
-}
+#variable "lambda_role_name" {
+#  description = "Name of the IAM role for the lambda function."
+#  type        = string
+#}
+
+#variable "function_name" {
+#  description = "Name of the Lambda function."
+#  type        = string
+#}
+
+#variable "handler" {
+#  description = "Handler for the Lambda function."
+#  type        = string
+#  default     = "handler.lambda_handler"
+#}
+
+#variable "runtime" {
+#  description = "Runtime for the Lambda function."
+#  type        = string
+#  default     = "python3.12"
+#}
+
+#variable "timeout" {
+#  description = "Timeout for the Lambda function in seconds."
+#  type        = number
+#  default     = 300
+#}
+
+#variable "memory_size" {
+#  description = "Memory size for the Lambda function in MB."
+#  type        = number
+#  default     = 256
+#}
+
+#variable "source_dir" {
+#  description = "Path to the Lmabda function source directory."
+#  type        = string
+#}
+
+#variable "environment_variables" {
+#  description = "Environmen variables for the Lambda function."
+#  type        = map(string)
+#  default     = {}
+#}
+
+#variable "tags" {
+#  description = "Tags to apply to the Lambda function."
+#  type        = map(string)
+#  default     = {}
+#}
+
+#variable "bucket_name" {
+#  description = "Name of the S3 bucket the Lambda function needs access to."
+#  type        = string
+#}
