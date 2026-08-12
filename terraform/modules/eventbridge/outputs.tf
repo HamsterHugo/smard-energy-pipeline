ee139766c27afb92feeea4b3e3db2dc2371350b6
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-12 15:32:58 +0200

    [ADD] terraform module for eventbridge.

--- terraform/modules/eventbridge/outputs.tf
+++ terraform/modules/eventbridge/outputs.tf
@@ -1,3 +1,7 @@
 output "rule_arn" {
   value = aws_cloudwatch_event_rule.schedule.arn
 }
+
+#output "rule_arn" {
+#  value = aws_cloudwatch_event_rule.schedule.arn
+#}
--- terraform/modules/eventbridge/variables.tf
+++ terraform/modules/eventbridge/variables.tf
@@ -4,23 +4,50 @@ variable "rule_name" {
 }
 
 variable "schedule_expression" {
-  description = "Schedule expression for the EventBridge rule."
+  description = "Schedule expression for the rule."
   type        = string
   default     = "cron(0 2 * * ? *)"
 }
 
 variable "lambda_arn" {
-  description = "ARN of the Lambda function to trigger."
+  description = "ARN of the Lambda Ingest function."
   type        = string
 }
 
 variable "lambda_function_name" {
-  description = "Name of the Lambda function."
+  description = "Name of the Lambda Ingest function."
   type        = string
 }
 
 variable "tags" {
-  description = "Tags to aplpy to the EventBridge rule."
+  description = "Tags to apply to all resources."
   type        = map(string)
   default     = {}
 }
+
+#variable "rule_name" {
+#  description = "Name of the EventBridge rule."
+#  type        = string
+#}
+
+#variable "schedule_expression" {
+#  description = "Schedule expression for the EventBridge rule."
+#  type        = string
+#  default     = "cron(0 2 * * ? *)"
+#}
+
+#variable "lambda_arn" {
+#  description = "ARN of the Lambda function to trigger."
+#  type        = string
+#}
+
+#variable "lambda_function_name" {
+#  description = "Name of the Lambda function."
+#  type        = string
+#}
+
+#variable "tags" {
+#  description = "Tags to aplpy to the EventBridge rule."
+#  type        = map(string)
+#  default     = {}
+#}
