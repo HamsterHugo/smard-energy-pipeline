e2c6ef0b9d77a624a3468372f4ca5c47c9e42c2f
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-14 09:59:28 +0200

    [FIX] API_URL with endpoint prod.

--- dashboard/frontend/script.js
+++ dashboard/frontend/script.js
@@ -4,7 +4,7 @@ const isLocal = window.location.hostname === 'localhost' ||
 
 const API_URL = isLocal
     ? 'http://localhost:5000'
-    : 'https://PLACEHOLDER.execute-api.us-east-1.amazonaws.com';
+    : 'https://PLACEHOLDER.execute-api.us-east-1.amazonaws.com/prod';
 
 const DATA_START_DATE = '2015-01-01';
 const NUCLEAR_END_DATE = '2023-04-16';
