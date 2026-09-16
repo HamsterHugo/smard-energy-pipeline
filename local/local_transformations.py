223b5a14c3ebc53788277a6fabd24cb5f618fdd5
Benedict Meinke <Benedict_Meinke@web.de>
2026-09-16 17:48:44 +0200

    [ADD] DOcstring, comments and logger messages to function process_and_store_timeseries.

--- local/local_transformations.py
+++ local/local_transformations.py
@@ -9,21 +9,42 @@ from smard_pipeline.transformations import get_current_timestamp
 
 logger = logging.getLogger(__name__)
 
-def process_and_store_single_timeseries(category: str, subcategory: str):
+def process_and_store_single_timeseries(category: str, subcategory: str) -> None:
+    """Processes and stores a single time series from the directory
+    'current_week' to the folder 'preprocessed_data'. This function shall be
+    used only for these time series that are not contained in the big table.
+
+
+    Args:
+        category (str): Name of the category.
+        subcategory (str): Name of the subcategory.
+    """
+    # Settings
     smard_id: int = CATEGORIES[category][subcategory]['id']
     current_timestamp: int = get_current_timestamp()
     file_name: str = f"{smard_id}_{current_timestamp}.parquet"
     file: Path = RAW_DATA_DIR / "current_week" / file_name
 
+    # Check, if the file exists. If not, end the function. 
     if not file.exists():
-        logger.warning(f'File {file} not found. You have to download it.')
+        logger.warning(f"File {file} not found. You have to download it.")
         return
+    else:
+        logger.info(f"File {file} found.")
 
-    df: pd.DataFrame = pd.read_parquet(file)
-    df.columns = ('timestamps', subcategory)
-    df['timestamps'] = convert_timestamp_column(df['timestamps'])
+    # Load the file.
+    try:
+        df: pd.DataFrame = pd.read_parquet(file)
+        df.columns = ('timestamps', subcategory)
+        df['timestamps'] = convert_timestamp_column(df['timestamps'])
+        logger.info("File loaded and processed successfully.")
 
-    output_dir: Path = PREPROCESSED_DATA_DIR / f"{smard_id}_current.parquet"
+        output_dir: Path = PREPROCESSED_DATA_DIR / f"{smard_id}_current.parquet"
 
-    df.to_parquet(output_dir, index=False)
-    logger.info(f'File saved: {output_dir.name}', extra={"status": "success"})
+        df.to_parquet(output_dir, index=False)
+        logger.info(f'File saved: {output_dir.name}', extra={"status": "success"})
+    except Exception as e:
+        logger.error(
+            f"Not able to load or process the file {file_name}.",
+            exc_info=e
+        )
